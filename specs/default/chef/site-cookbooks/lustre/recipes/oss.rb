1f4b2195535ecbc495f08313039c7945c525a11c
hugo meiland <hugo.meiland@microsoft.com>
2019-03-14 15:44:25 -0700

    add index to lustre oss

--- specs/default/chef/site-cookbooks/lustre/recipes/oss.rb
+++ specs/default/chef/site-cookbooks/lustre/recipes/oss.rb
@@ -5,24 +5,21 @@ include_recipe "::default"
 
 %w{lustre kmod-lustre-osd-ldiskfs lustre-dkms lustre-osd-ldiskfs-mount lustre-resource-agents e2fsprogs lustre-tests}.each { |p| package p }
 
-meta_directory = "#{node["lustre"]["root_dir"]}/meta"
-directory "#{meta_directory}" do
-    recursive true
-end
-
 #manager_ipaddress = ::Lustre::Helpers.search_for_manager(node['cyclecloud']['cluster']['id'])
 manager_ipaddress = node["lustre"]["manager_ipaddress"]
 
 chef_state =  node['cyclecloud']['chefstate']
 lustre_meta_conf_file = "/etc/lustre/lustre-meta.conf"
 hostname_line = "sysMgmtdHost = #{manager_ipaddress}"
+ost_index = node[:azure][:metadata][:compute][:name].split('_')[1]
 
-bash 'initialize mds' do
+bash 'initialize oss' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
   echo "connecting to mds #{manager_ipaddress}"
-  mkfs.lustre --ost --fsname lustre --mgsnode #{manager_ipaddress}@tcp0 /dev/nvme0n1
+  echo "creating oss index #{ost_index}"
+  mkfs.lustre --ost --fsname lustre --mgsnode #{manager_ipaddress}@tcp0 --index=#{ost_index} /dev/nvme0n1
   mkdir -p /mnt/ost/
   mount -t lustre /dev/nvme0n1 /mnt/ost/
   EOH
