83196b04e862d4b4910256fef0fd26f9018a33df
hugo meiland <hugo.meiland@microsoft.com>
2019-03-23 19:53:54 +0100

    added hsm support

---
+++ specs/default/chef/site-cookbooks/lustre/recipes/hsm.rb
@@ -0,0 +1,83 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+include_recipe "::default"
+
+%w{git lustre-client lustre-client-dkms}.each { |p| package p }
+manager_ipaddress = node["lustre"]["manager_ipaddress"]
+
+bash 'build hsm' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+yum install -y https://github.com/whamcloud/lemur/releases/download/0.5.2/lhsm-0.5.2-1.x86_64.rpm https://github.com/whamcloud/lemur/releases/download/0.5.2/lemur-data-movers-0.5.2-1.x86_64.rpm https://github.com/whamcloud/lemur/releases/download/0.5.2/lemur-hsm-agent-0.5.2-1.x86_64.rpm https://github.com/whamcloud/lemur/releases/download/0.5.2/lemur-testing-0.5.2-1.x86_64.rpm
+wget https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.12.1.linux-amd64.tar.gz
+export PATH=/usr/local/go/bin:$PATH
+go get -u github.com/edwardsp/lemur/cmd/lhsm-plugin-az
+go build github.com/edwardsp/lemur/cmd/lhsm-plugin-az
+sudo cp lhsm-plugin-az /usr/libexec/lhsmd/.
+  EOH
+end
+
+directory '/var/run/lhsmd' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/etc/lhsmd' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+directory '/lustre' do
+  owner 'root'
+  group 'root'
+  mode '0777'
+  action :create
+end
+
+template '/etc/lhsmd/agent' do
+  source 'hsm/agent.erb'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
+
+template '/etc/lhsmd/lhsm-plugin-az' do
+  source 'hsm/lhsm-plugin-az.erb'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
+
+template '/etc/systemd/system/lhsmd.service' do
+  source 'hsm/lhsmd.service.erb'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
+bash 'start lhsmd' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+systemctl daemon-reload
+systemctl enable lhsmd
+systemctl start lhsmd
+  EOH
+end
+#mount -t lustre #{manager_ipaddress}@tcp0:/lustre /lustre
+#if File.read('/etc/mtab').lines.grep(/ lustre /)[0]
+#  Chef::Log.info("Lustre is already mounted")
+#else
+#  mount '/lustre' do
+#    device "#{manager_ipaddress}@tcp0:/lustre"
+#    fstype 'lustre'
+    #options 'rw'
+#    action [:mount, :enable]
+#  end
+#end
+
--- specs/default/chef/site-cookbooks/lustre/recipes/mds.rb
+++ specs/default/chef/site-cookbooks/lustre/recipes/mds.rb
@@ -12,10 +12,14 @@ bash 'initialize mds' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-  echo "my ip is #{manager_ipaddress}"
-  mkfs.lustre --fsname=lustre --mgs --mdt --index=0 /dev/nvme0n1
-  mkdir -p /mnt/mdsmgs
-  mount -t lustre /dev/nvme0n1 /mnt/mdsmgs
+echo "my ip is #{manager_ipaddress}"
+mkfs.lustre --fsname=lustre --mgs --mdt --index=0 /dev/nvme0n1
+mkdir -p /mnt/mdsmgs
+mount -t lustre /dev/nvme0n1 /mnt/mdsmgs
+sleep 20
+lctl set_param -P mdt.*-MDT0000.hsm_control=enabled
+lctl set_param -P mdt.*-MDT0000.hsm.default_archive_id=1
+lctl set_param mdt.*-MDT0000.hsm.max_requests=128
   EOH
 end
 
