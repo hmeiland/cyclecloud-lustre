2eb2324e8fe7dec2a7026313fa20c722175825dd
hugo meiland <hugo.meiland@microsoft.com>
2019-04-16 10:50:18 +0200

    variable mountpoint for client and autostart single oss on cluster start

--- specs/default/chef/site-cookbooks/lustre/recipes/client.rb
+++ specs/default/chef/site-cookbooks/lustre/recipes/client.rb
@@ -5,16 +5,14 @@ include_recipe "::default"
 %w{lustre-client lustre-client-dkms}.each { |p| package p }
 manager_ipaddress = node["lustre"]["manager_ipaddress"]
 
-bash 'initialize client' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-  echo "my ip is #{manager_ipaddress}"
-  #mkdir -p /lustre
-  EOH
+if node["lustre"]["client"]["mount_point"]
+  mount_point = node["lustre"]["client"]["mount_point"]
+else
+  mount_point = "/lustre" 
 end
 
-directory '/lustre' do
+#directory '/lustre' do
+directory "#{mount_point}" do
   owner 'root'
   group 'root'
   mode '0777'
@@ -25,7 +23,8 @@ end
 if File.read('/etc/mtab').lines.grep(/ lustre /)[0]
   Chef::Log.info("Lustre is already mounted")
 else
-  mount '/lustre' do
+  #mount '/lustre' do
+  mount "#{mount_point}" do
     device "#{manager_ipaddress}@tcp0:/lustre"
     fstype 'lustre'
     #options 'rw'
